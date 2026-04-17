4bfb2abf65b5fe9a780a5a994cc6900de1822449
Pubgggdie <91722397+Pubgggdie@users.noreply.github.com>
2026-04-17 16:37:48 +0530

    feat: update search tool to use MAX_CHARACTERS constant and adjust maxCharacters limit

--- src/tools/get-search.js
+++ src/tools/get-search.js
@@ -2,6 +2,8 @@ import { tool, zodSchema } from "ai";
 import { getExa } from "../utils/ai.js";
 import { z } from "zod";
 
+
+const MAX_CHARACTERS = 980;
 export const searchTool = tool({
   description: "Search for information on the internet and return relevant results.",
   inputSchema: zodSchema(
@@ -14,15 +16,17 @@ export const searchTool = tool({
 
     const result = await exa.search(query, {
       type: "fast",
+     numResults: 3,
       contents: {
         highlights: {
-          maxCharacters: 4000,
+          maxCharacters: MAX_CHARACTERS,
         },
+    
       },
     });
 
     return {
-      results: (result.results || [])
+      results: (result.results)
     };
   },
 });
