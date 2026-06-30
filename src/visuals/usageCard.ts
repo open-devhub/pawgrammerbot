7ecdc9d8a062db263a4d871bd60de6e205be923a
Caleb <quantumpandaprog@gmail.com>
2026-06-30 13:11:31 +0300

    fix: wrong path to fonts

--- src/visuals/usageCard.ts
+++ src/visuals/usageCard.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const FONTS_DIR = path.join(__dirname, "..", "assets", "fonts");
+const FONTS_DIR = path.join(__dirname, "..", "..", "assets", "fonts");
 
 let fontsRegistered = false;
 function ensureFonts() {
